ac36f7cc111d34ee2613eace92c7cf26c4c0e680
Hampus Näsström <hampus.nasstrom@gmail.com>
2023-11-27 17:46:19 +0100

    Added Check if Steps are After Each Other

--- src/nomad_material_processing/__init__.py
+++ src/nomad_material_processing/__init__.py
@@ -18,6 +18,7 @@
 from typing import (
     TYPE_CHECKING,
 )
+import numpy as np
 from nomad.metainfo import (
     Package,
     Quantity,
@@ -314,6 +315,29 @@ class SampleDeposition(SynthesisMethod):
             "http://purl.obolibrary.org/obo/CHMO_0001310"
         ],)
 
+    def is_serial(self) -> bool:
+        '''
+        Method for determining if the steps are serial. Can be overwritten by sub class.
+        Default behavior is to return True if all steps start after the previous one.
+
+        Returns:
+            bool: Whether or not the steps are serial.
+        '''
+        start_times = []
+        durations = []
+        for step in self.steps:
+            if step.start_time is None or step.duration is None:
+                return False
+            start_times.append(step.start_time.timestamp())
+            durations.append(step.duration.to('s').magnitude)
+        start_times = np.array(start_times)
+        durations = np.array(durations)
+        end_times = start_times + durations
+        diffs = start_times[1:] - end_times[:-1]
+        if np.any(diffs < 0):
+            return False
+        return True
+
     def normalize(self, archive: 'EntryArchive', logger: 'BoundLogger') -> None:
         '''
         The normalizer for the `SampleDeposition` class.
@@ -323,17 +347,17 @@ class SampleDeposition(SynthesisMethod):
             normalized.
             logger (BoundLogger): A structlog logger.
         '''
-        super(SampleDeposition, self).normalize(archive, logger)
-        tasks = []
-        previous = None
-        for step in self.steps:
-            task = step.to_task()
-            task.outputs = [Link(name=step.name, section=step)]
-            if previous is not None:
-                task.inputs = [Link(name=previous.name, section=previous)]
-            tasks.append(task)
-            previous=step
-        archive.workflow2.tasks = tasks
+        if self.is_serial():
+            tasks = []
+            previous = None
+            for step in self.steps:
+                task = step.to_task()
+                task.outputs = [Link(name=step.name, section=step)]
+                if previous is not None:
+                    task.inputs = [Link(name=previous.name, section=previous)]
+                tasks.append(task)
+                previous=step
+            archive.workflow2.tasks = tasks
 
 
 m_package.__init_metainfo__()
