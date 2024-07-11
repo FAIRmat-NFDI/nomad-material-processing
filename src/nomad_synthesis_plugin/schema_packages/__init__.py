e8f4173252ab72eb16c5639a8b90f020a3903c6f
Sebastian Brueckner <sebasdaszebra@gmail.com>
2024-07-11 20:01:15 +0200

    add synthesis schema and reader

--- src/nomad_synthesis_plugin/schema_packages/__init__.py
+++ src/nomad_synthesis_plugin/schema_packages/__init__.py
@@ -7,11 +7,26 @@ class MySchemaPackageEntryPoint(SchemaPackageEntryPoint):
 
     def load(self):
         from nomad_synthesis_plugin.schema_packages.mypackage import m_package
- 
+
         return m_package
 
 
 mypackage = MySchemaPackageEntryPoint(
     name='MyPackage',
+    description='Schema package defined using the new plugin mechanism.',
+)
+
+
+class CrystallizationPackageEntryPoint(SchemaPackageEntryPoint):
+    parameter: int = Field(0, description='Custom configuration parameter')
+
+    def load(self):
+        from nomad_synthesis_plugin.schema_packages.crystallization import m_package
+
+        return m_package
+
+
+crystallization = CrystallizationPackageEntryPoint(
+    name='Crystallization',
     description='Schema package defined using the new plugin mechanism.',
 )
