eac35b1d38c50687a94e6bab55644a1715317b6f
Sarthak Kapoor <57119427+ka-sarthak@users.noreply.github.com>
2024-08-12 16:09:33 +0200

    52 test for vapor deposition (#53)
    * Update section paths
    
    * Remove test for non-existing section
    
    * Ruff test_schema.py

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -3,8 +3,11 @@ import glob
 
 from nomad.client import parse, normalize_all
 
+
 def test_schema():
-    test_files = glob.glob(os.path.join(os.path.dirname(__file__), 'data', '*.archive.yaml'))
+    test_files = glob.glob(
+        os.path.join(os.path.dirname(__file__), 'data', '*.archive.yaml')
+    )
     for test_file in test_files:
         entry_archive = parse(test_file)[0]
         normalize_all(entry_archive)
