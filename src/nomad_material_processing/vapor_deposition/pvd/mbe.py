476c948e98242721f1b893f4a169addc1db76571
Andrea Albino <95371554+aalbino2@users.noreply.github.com>
2024-08-21 10:05:56 +0200

    generated mbe file (#60)

---
+++ src/nomad_material_processing/vapor_deposition/pvd/mbe.py
@@ -0,0 +1,57 @@
+#
+# Copyright The NOMAD Authors.
+#
+# This file is part of NOMAD. See https://nomad-lab.eu for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from nomad.metainfo import (
+    Package,
+    Quantity,
+    Section,
+)
+from nomad_material_processing.vapor_deposition.pvd import (
+    PhysicalVaporDeposition,
+)
+
+
+class MolecularBeamEpitaxy(PhysicalVaporDeposition):
+    """
+    A synthesis method which consists of depositing a monocrystalline film (from a
+    molecular beam) on a monocrystalline substrate under high vacuum (<10^{-8} Pa).
+    Molecular beam epitaxy is very slow, with a deposition rate of <1000 nm per hour.
+    [database_cross_reference: https://orcid.org/0000-0002-0640-0422]
+
+    Synonyms:
+     - MBE
+     - molecular-beam epitaxy
+    """
+
+    m_def = Section(
+        links=[
+            'http://purl.obolibrary.org/obo/CHMO_0001336',
+            'http://purl.obolibrary.org/obo/CHMO_0001341',
+        ],
+    )
+
+    def normalize(self, archive: 'EntryArchive', logger: 'BoundLogger') -> None:
+        """
+        The normalizer for the `MolecularBeamEpitaxy` class.
+
+        Args:
+            archive (EntryArchive): The archive containing the section that is being
+            normalized.
+            logger (BoundLogger): A structlog logger.
+        """
+        super(MolecularBeamEpitaxy, self).normalize(archive, logger)
