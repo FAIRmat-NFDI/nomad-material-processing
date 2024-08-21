a011c640bbe9241b8c60e2fdbf481d5867a63a24
Hampus Näsström <hampus.nasstrom@gmail.com>
2024-08-21 09:59:18 +0200

    47 refactor combinatorial sample to reference library (#67)
    * Changed combinatorial reference order
    
    * Added plot functionality to CombinatorialLibrary class
    
    * Changed to reference section for library in sample

--- src/nomad_material_processing/combinatorial.py
+++ src/nomad_material_processing/combinatorial.py
@@ -1,3 +1,5 @@
+import json
+
 #
 # Copyright The NOMAD Authors.
 #
@@ -18,6 +20,11 @@
 from typing import (
     TYPE_CHECKING,
 )
+import plotly.graph_objects as go
+from nomad.datamodel.metainfo.plot import (
+    PlotlyFigure,
+    PlotSection,
+)
 from nomad.metainfo import (
     Package,
     Quantity,
@@ -26,6 +33,7 @@ from nomad.metainfo import (
 )
 from nomad.datamodel.data import (
     ArchiveSection,
+    EntryData,
 )
 from nomad.datamodel.metainfo.annotations import (
     ELNAnnotation,
@@ -36,6 +44,7 @@ from nomad.datamodel.metainfo.basesections import (
     CompositeSystem,
     CompositeSystemReference,
 )
+
 if TYPE_CHECKING:
     from nomad.datamodel.datamodel import (
         EntryArchive,
@@ -47,142 +56,106 @@ if TYPE_CHECKING:
 m_package = Package(name='Combinatorial Synthesis')
 
 
-class CombinatorialSample(CompositeSystem):
-    '''
-    A base section for any sample of a combinatorial library.
-    '''
+class CombinatorialLibrary(CompositeSystem, EntryData, PlotSection):
+    """
+    A base section for any continuous combinatorial library.
+    """
+
     m_def = Section()
-    sample_number = Quantity(
-        type=int,
-        description='''
-        A unique number for this sample of the combinatorial library.
-        ''',
-        a_eln=ELNAnnotation(
-            component=ELNComponentEnum.NumberEditQuantity,
-        ),
-    )
-    lab_id = Quantity(
-        type=str,
-        description='''
-        A unique human readable ID for the sample within the combinatorial library.
-        Suggested to be the ID of the library followed by a dash ("-") and the sample
-        number.
-        ''',
-        a_eln=ELNAnnotation(
-            component=ELNComponentEnum.StringEditQuantity,
-            label='Sample ID',
-        ),
-    )
+
+    def plot(self, archive: 'EntryArchive', logger: 'BoundLogger') -> None:
+        from nomad.search import (
+            search,
+            MetadataPagination,
+        )
+
+        query = {
+            'section_defs.definition_qualified_name:all': [
+                'nomad_material_processing.combinatorial.CombinatorialSample'
+            ],
+            'entry_references.target_entry_id:all': [archive.metadata.entry_id],
+        }
+        search_result = search(
+            owner='all',
+            query=query,
+            pagination=MetadataPagination(page_size=1),
+            user_id=archive.metadata.main_author.user_id,
+        )
+        references = []
+        x_values = []
+        y_values = []
+        z_values = []
+        if search_result.pagination.total > 0:
+            for res in search_result.data:
+                entry_id = res['entry_id']
+                upload_id = res['upload_id']
+                reference = f'../../../{upload_id}/entry/id/{entry_id}'
+                x, y, z = None, None, None
+                for quantity in res['search_quantities']:
+                    if quantity['path_archive'] == 'data.position.x':
+                        x = quantity['float_value']
+                    if quantity['path_archive'] == 'data.position.y':
+                        y = quantity['float_value']
+                    if quantity['path_archive'] == 'data.position.z':
+                        z = quantity['float_value']
+                references.append(reference)
+                x_values.append(x)
+                y_values.append(y)
+                z_values.append(z)
+            print(f'Found {search_result.pagination.total} activities.')
+        fig = go.Figure(
+            data=go.Scatter(
+                x=x_values,
+                y=y_values,
+                mode='markers',
+                customdata=references,
+                marker=dict(color='#2A4CDF'),
+                hovertemplate='<a href="%{customdata}">Link</a><extra></extra>',
+            )
+        )
+
+        # Set plot title and axis labels
+        fig.update_layout(
+            template='plotly_white',
+            hovermode='closest',
+            dragmode='zoom',
+            title='Scatter Plot of x and y Coordinates',
+            xaxis_title='x / m',
+            yaxis_title='y / m',
+        )
+        plot_json = fig.to_plotly_json()
+        plot_json['config'] = dict(
+            scrollZoom=False,
+        )
+        self.figures.append(
+            PlotlyFigure(
+                label='Power, pressure, and temperature',
+                figure=plot_json,
+            )
+        )
 
     def normalize(self, archive: 'EntryArchive', logger: 'BoundLogger') -> None:
-        '''
-        The normalizer for the `CombinatorialSample` section.
+        """
+        The normalizer for the `ContinuousCombiLibrary` section.
 
         Args:
             archive (EntryArchive): The archive containing the section that is being
             normalized.
             logger (BoundLogger): A structlog logger.
-        '''
-        super(CombinatorialSample, self).normalize(archive, logger)
-
-
-class CombinatorialSampleReference(CompositeSystemReference):
-    '''
-    A section containing a reference to a combinatorial sample entry.
-    '''
-    m_def = Section(
-        label_quantity='sample_number',
-    )
-    sample_number = Quantity(
-        type=int,
-        description='''
-        A unique number for this sample of the combinatorial library.
-        ''',
-        a_eln=ELNAnnotation(
-            component=ELNComponentEnum.NumberEditQuantity,
-        ),
-    )
-    reference = Quantity(
-        type=CombinatorialSample,
-        description='''
-        The reference to the combinatorial sample entry.
-        ''',
-        a_eln=ELNAnnotation(
-            component=ELNComponentEnum.ReferenceEditQuantity,
-            label='Sample Reference',
-        ),
-    )
-
-    def normalize(self, archive: 'EntryArchive', logger: 'BoundLogger') -> None:
-        '''
-        The normalizer for the `CombinatorialSampleReference` section.
-
-        Args:
-            archive (EntryArchive): The archive containing the section that is being
-            normalized.
-            logger (BoundLogger): A structlog logger.
-        '''
-        super(CombinatorialSampleReference, self).normalize(archive, logger)
-
-
-class CombinatorialLibrary(Collection):
-    '''
-    A base section for any combinatorial library.
-    '''
-    m_def = Section()
-    lab_id = Quantity(
-        type=str,
-        description='''
-        A unique human readable ID for the combinatorial library.
-        ''',
-        a_eln=ELNAnnotation(
-            component=ELNComponentEnum.StringEditQuantity,
-            label='Library ID'
-        ),
-    )
-    entities = SubSection(
-        section_def=CombinatorialSampleReference,
-        description='''
-        All the investigated samples of the combinatorial library.
-        ''',
-        repeats=True,
-        a_eln=ELNAnnotation(
-            label='Samples'
-        ),
-    )
-    # lab_id = Collection.lab_id.m_copy()
-    # lab_id.description = '''
-    #     A unique human readable ID for the combinatorial library.
-    #     '''
-    # lab_id.m_annotations['eln'].label = 'Library ID'
-    # entities = Collection.entities.m_copy()
-    # entities.section_def = CombinatorialSampleReference
-    # entities.description = '''
-    #     All the investigated samples of the combinatorial library.
-    #     '''
-    # entities.m_annotations['eln'] = ELNAnnotation(
-    #     label='Samples'
-    # )  # Currently the label cannot be overwritten.
-
-    def normalize(self, archive: 'EntryArchive', logger: 'BoundLogger') -> None:
-        '''
-        The normalizer for the `CombinatorialLibrary` section.
-
-        Args:
-            archive (EntryArchive): The archive containing the section that is being
-            normalized.
-            logger (BoundLogger): A structlog logger.
-        '''
+        """
         super(CombinatorialLibrary, self).normalize(archive, logger)
+        self.figures = []
+        self.plot(archive, logger)
 
 
 class CombinatorialSamplePosition(ArchiveSection):
-    '''
+    """
     A section for representing the position of a sample within a continuous
     combinatorial library.
     If nothing else is specified it is the position relative to the center of mass of
     the library.
-    '''
+    """
+
     m_def = Section()
     x = Quantity(
         type=float,
@@ -210,109 +183,185 @@ class CombinatorialSamplePosition(ArchiveSection):
     )
 
     def normalize(self, archive: 'EntryArchive', logger: 'BoundLogger') -> None:
-        '''
+        """
         The normalizer for the `CombinatorialSamplePosition` section.
 
         Args:
             archive (EntryArchive): The archive containing the section that is being
             normalized.
             logger (BoundLogger): A structlog logger.
-        '''
+        """
         super(CombinatorialSamplePosition, self).normalize(archive, logger)
 
 
-class ContinuousCombiSample(CombinatorialSample):
-    '''
+class CombinatorialLibraryReference(CompositeSystemReference):
+    """
+    A section containing a reference to a continuous combinatorial library entry.
+    """
+
+    m_def = Section(
+        label_quantity='lab_id',
+    )
+    reference = Quantity(
+        type=CombinatorialLibrary,
+        description="""
+        The reference to the combinatorial library entry.
+        """,
+        a_eln=ELNAnnotation(
+            component=ELNComponentEnum.ReferenceEditQuantity,
+            label='Library Reference',
+        ),
+    )
+
+
+class CombinatorialSample(CompositeSystem, EntryData):
+    """
     A base section for any sample of a continuous combinatorial library.
-    '''
+    """
+
     m_def = Section()
+    sample_number = Quantity(
+        type=int,
+        description="""
+        A unique number for this sample of the combinatorial library.
+        """,
+        a_eln=ELNAnnotation(
+            component=ELNComponentEnum.NumberEditQuantity,
+        ),
+    )
+    lab_id = Quantity(
+        type=str,
+        description="""
+        A unique human readable ID for the sample within the combinatorial library.
+        Suggested to be the ID of the library followed by a dash ("-") and the sample
+        number.
+        """,
+        a_eln=ELNAnnotation(
+            component=ELNComponentEnum.StringEditQuantity,
+            label='Sample ID',
+        ),
+    )
+    library = SubSection(
+        section_def=CombinatorialLibraryReference,
+        description="""
+        The reference to the combinatorial library entry.
+        """,
+    )
     position = SubSection(
         section_def=CombinatorialSamplePosition,
-        description='''
-        The position of a sample within the continuous combinatorial library. If nothing 
-        else is specified it is the position relative to the center of mass of the 
+        description="""
+        The position of a sample within the continuous combinatorial library. If nothing
+        else is specified it is the position relative to the center of mass of the
         library.
-        ''',
+        """,
     )
 
     def normalize(self, archive: 'EntryArchive', logger: 'BoundLogger') -> None:
-        '''
-        The normalizer for the `ContinuousCombiSample` section.
+        """
+        The normalizer for the `CombinatorialSample` section.
 
         Args:
             archive (EntryArchive): The archive containing the section that is being
             normalized.
             logger (BoundLogger): A structlog logger.
-        '''
-        super(ContinuousCombiSample, self).normalize(archive, logger)
+        """
+        super(CombinatorialSample, self).normalize(archive, logger)
 
 
-class ContinuousCombiSampleReference(CombinatorialSampleReference):
-    '''
-    A section containing a reference to a continuous combinatorial sample entry.
-    '''
+# Discrete combinatorial library classes:
+class DiscreteCombinatorialSample(CompositeSystem):
+    """
+    A base section for any sample of a discrete combinatorial library.
+    """
+
+    m_def = Section()
+
+    def normalize(self, archive: 'EntryArchive', logger: 'BoundLogger') -> None:
+        """
+        The normalizer for the `CombinatorialSample` section.
+
+        Args:
+            archive (EntryArchive): The archive containing the section that is being
+            normalized.
+            logger (BoundLogger): A structlog logger.
+        """
+        super(DiscreteCombinatorialSample, self).normalize(archive, logger)
+
+
+class DiscreteCombinatorialSampleReference(CompositeSystemReference):
+    """
+    A section containing a reference to a discrete combinatorial sample entry.
+    """
+
     m_def = Section(
         label_quantity='sample_number',
     )
+    sample_number = Quantity(
+        type=int,
+        description="""
+        A unique number for this sample of the combinatorial library.
+        """,
+        a_eln=ELNAnnotation(
+            component=ELNComponentEnum.NumberEditQuantity,
+        ),
+    )
     reference = Quantity(
-        type=ContinuousCombiSample,
-        description='''
+        type=DiscreteCombinatorialSample,
+        description="""
         The reference to the combinatorial sample entry.
-        ''',
+        """,
         a_eln=ELNAnnotation(
             component=ELNComponentEnum.ReferenceEditQuantity,
             label='Sample Reference',
         ),
     )
-    # reference = CombinatorialSampleReference.reference.m_copy()
-    # reference.type = ContinuousCombiSample  # Specializing custom types is currently not supported.
-    position = SubSection(
-        section_def=CombinatorialSamplePosition,
-        description='''
-        The position of a sample within the continuous combinatorial library. If nothing 
-        else is specified it is the position relative to the center of mass of the 
-        library.
-        ''',
-    )
 
     def normalize(self, archive: 'EntryArchive', logger: 'BoundLogger') -> None:
-        '''
-        The normalizer for the `ContinuousCombiSampleReference` section.
+        """
+        The normalizer for the `CombinatorialSampleReference` section.
 
         Args:
             archive (EntryArchive): The archive containing the section that is being
             normalized.
             logger (BoundLogger): A structlog logger.
-        '''
-        super(ContinuousCombiSampleReference, self).normalize(archive, logger)
+        """
+        super(DiscreteCombinatorialSampleReference, self).normalize(archive, logger)
 
 
-class ContinuousCombiLibrary(CombinatorialLibrary):
-    '''
-    A base section for any continuous combinatorial library.
-    '''
+class DiscreteCombinatorialLibrary(Collection):
+    """
+    A base section for a discrete combinatorial library.
+    """
+
     m_def = Section()
-    entities = SubSection(
-        section_def=ContinuousCombiSampleReference,
-        description='''
-        All the investigated samples of the combinatorial library.
-        ''',
-        repeats=True,
+    lab_id = Quantity(
+        type=str,
+        description="""
+        A unique human readable ID for the combinatorial library.
+        """,
         a_eln=ELNAnnotation(
-            label='Samples'
+            component=ELNComponentEnum.StringEditQuantity, label='Library ID'
         ),
     )
+    entities = SubSection(
+        section_def=DiscreteCombinatorialSampleReference,
+        description="""
+        All the investigated samples of the combinatorial library.
+        """,
+        repeats=True,
+        a_eln=ELNAnnotation(label='Samples'),
+    )
 
     def normalize(self, archive: 'EntryArchive', logger: 'BoundLogger') -> None:
-        '''
-        The normalizer for the `ContinuousCombiLibrary` section.
+        """
+        The normalizer for the `DiscreteCombinatorialLibrary` section.
 
         Args:
             archive (EntryArchive): The archive containing the section that is being
             normalized.
             logger (BoundLogger): A structlog logger.
-        '''
-        super(ContinuousCombiLibrary, self).normalize(archive, logger)
+        """
+        super(DiscreteCombinatorialLibrary, self).normalize(archive, logger)
 
 
 m_package.__init_metainfo__()
