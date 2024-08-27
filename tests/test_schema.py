a3e73d36553c9e0f170f9b8e7ffad863938f132b
Andrea Albino <andrea.albino@physik.hu-berlin.de>
2024-08-27 10:04:30 +0200

    test

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -1,15 +1,15 @@
 import glob
 import os.path
-import sys
 
+import pytest
 from nomad.client import normalize_all, parse
 
+test_files = glob.glob(
+    os.path.join(os.path.dirname(__file__), 'data', '*.archive.yaml')
+)
 
-def test_schema():
-    test_files = glob.glob(
-        os.path.join(os.path.dirname(__file__), 'data', '*.archive.yaml')
-    )
-    for test_file in test_files:
-        sys.stdout.write(f'Test file: {test_file}')
-        entry_archive = parse(test_file)[0]
-        normalize_all(entry_archive)
+
+@pytest.mark.parametrize('test_file', test_files)
+def test_schema(test_file):
+    entry_archive = parse(test_file)[0]
+    normalize_all(entry_archive)
