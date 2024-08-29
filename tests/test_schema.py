61f707e0057c865fc118028c76c8625241e35d12
Andrea Albino <andrea.albino@physik.hu-berlin.de>
2024-08-29 10:10:37 +0200

    formatting

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -8,6 +8,7 @@ test_files = glob.glob(
     os.path.join(os.path.dirname(__file__), 'data', '*.archive.yaml')
 )
 
+
 @pytest.mark.parametrize('test_file', test_files)
 def test_schema(test_file):
     entry_archive = parse(test_file)[0]
